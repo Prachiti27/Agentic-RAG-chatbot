6df60d7f51f5f8ec39cd2697a4aa943c08e976ed
Prachiti Kitey <prachitikitey86@gmail.com>
2026-02-11 14:30:46 +0530

    feat: doc ingestion

---
+++ src/rag_doc_ingestion/config/doc_ingestion_settings.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+class DocIngestionSettings(BaseSettings):
+    DOCUMENTS_DIR: str
+    VECTOR_STORE_DIR: str
+    COLLECTION_NAME: str
+    
+    class Config: 
+        env_file = ".env"
+        env_File_encoding = "utf-8"
+        extra = "allow"
